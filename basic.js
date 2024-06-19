71e2f676805f052b8eebe714ba5834806b2085c8
Pouyan <154380480+Mrengin1@users.noreply.github.com>
2024-06-19 16:20:13 -0400

    Update basic.js

--- basic.js
+++ basic.js
@@ -4,3 +4,9 @@
 //How to fill up an H1 tag with js👇🏼
 //first you have to chose an ID for your h1 tag and then write this in the js file
 document.getElementById("myH1").textContent = 'hello';
+
+//to find a type of something we use the method (typeof)
+let price = 100
+console.log(typeof price);
+
+
