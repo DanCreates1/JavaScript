15d37245adeb35df45647756d05056c5b24a9d1e
Pouyan <154380480+Mrengin1@users.noreply.github.com>
2024-06-24 15:33:04 -0400

    Update basic.js

--- basic.js
+++ basic.js
@@ -335,6 +335,31 @@ if(temp > 0 && temp < 30 && sunny){
 else{
    console.log('The weather is bad!');
 }
+
+
+
+
+
+// Not ! logical operator
+// typically used to reverse a condition's boolean value
+// true -> false false -> true 
+
+let temp = 16;
+let sunny = true;
+
+if(!(temp > 15)){
+   console.log("It is cold outside");
+}
+else{
+   console.log("It is warm outside");
+}
+
+if(!sunny){
+   console.log("It is cloudy outside");
+}
+else{
+   console.log("It's sunny outside");
+}
 else{
    console.log('The weather is Good!');
 }
