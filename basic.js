16bed21b0a1aa04ba079de6139ea9e75fa6333bb
Pouyan <154380480+Mrengin1@users.noreply.github.com>
2024-06-26 22:00:59 -0400

    Update basic.js

--- basic.js
+++ basic.js
@@ -458,3 +458,18 @@ for(let i = 1; i <= rows; i+=1){
    }
    document.getElementById("myrectangel").innerHTML += "<br>";
 }
+
+
+
+
+//nested loop = a loop inside of another loop
+let symbol = window.prompt("Enter a symbol to use")
+let rows = window.prompt('Enter # of rows')
+let columns = window.prompt('Enter # of columns')
+
+for(let i = 1; i <= rows; i+=1){
+   for(let j = 1; j <= columns; j+=1){
+      document.getElementById("myrectangel").innerHTML +=symbol;
+   }
+   document.getElementById("myrectangel").innerHTML += "<br>";
+}
