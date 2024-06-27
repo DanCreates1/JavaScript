d81fd71baf6ea71a9deef97a354041916b1c5bbd
Pouyan <154380480+Mrengin1@users.noreply.github.com>
2024-06-27 18:52:03 -0400

    Update basic.js

--- basic.js
+++ basic.js
@@ -566,6 +566,14 @@ let userName = "Dan";
 let items = 3;
 let total = 75;
 
+
+
 console.log("Hello", userName);
 console.log("You have", items, "items in your cart");
 console.log("Your total is $", total);
+
+console.log("--------------------------------")
+
+console.log(`Hello ${userName}`);
+console.log(`You have ${items} items in your cart`);
+console.log(`Your total is $${total}`);
