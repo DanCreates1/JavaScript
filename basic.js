9339ac1b5b6e6e850596188f1e608ffe3a0a1d89
Pouyan <154380480+Mrengin1@users.noreply.github.com>
2024-06-26 22:16:39 -0400

    Update basic.js

--- basic.js
+++ basic.js
@@ -490,3 +490,23 @@ function happyBirthday(){
    console.log("happy birthday to you")
    console.log("You are", age,"years old")
 }
+
+
+
+//return = returns a value back to the place where you invoked a function
+
+let area;
+let width;
+let height;
+
+width = window.prompt("Enter width");
+height = window.prompt("Enter height");
+
+
+area = getArea(width, height);
+
+console.log("The area is:", area)
+
+function getArea(width, height){
+   return width * height;
+}
