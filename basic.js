ee1b4c45cddbe00d6926aed7407d28fc6e20443d
Pouyan <154380480+Mrengin1@users.noreply.github.com>
2024-06-28 13:50:35 -0400

    Update basic.js

--- basic.js
+++ basic.js
@@ -601,8 +601,12 @@ console.log(text)
 //'locale' = specify that language (undefined = default set in browser)
 // 'options' = object with formatting options
 
-let myNum = 123456.789;
+let myNum = 100;
 
 //myNum = myNum.toLocaleString("en-US"); //US English
 //myNum = myNum.toLocaleString("hi-IN"); // Hindi
 //myNum = myNum.toLocaleString("de-DE"); // Standard German
+
+//myNum = myNum.toLocaleString("en-US", {style: "currency", currency: "USD"})
+//myNum = myNum.toLocaleString("hi-IN", {style: "currency", currency: "INR"})
+//myNum = myNum.toLocaleString("de-DE", {style: "currency", currency: "EUR"})
