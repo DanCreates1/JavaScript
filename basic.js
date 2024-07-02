ce49843e9a4cfe22f3a05fc76012a58b2e8aad98
Pouyan <154380480+Mrengin1@users.noreply.github.com>
2024-07-02 14:28:05 -0400

    Update basic.js

--- basic.js
+++ basic.js
@@ -723,3 +723,19 @@ function sum(x, y, ...numbers){
    }
    return total
 }
+
+
+
+//array.forEach() = executes a provided callback function
+//                  once for each array element
+
+let students = ["spongrbob", "patrick", "squidward"];
+students.forEach(capitalize);
+students.forEach(print);
+
+function capitalize(element, index, array){
+   array[index] = element[0].toUpperCase() + element.substring(1);
+}
+function print(element){
+   console.log(element)
+}
