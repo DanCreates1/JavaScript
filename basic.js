63b38a45b46cb0152643b7db45d839608262db88
Pouyan <154380480+Mrengin1@users.noreply.github.com>
2024-07-02 16:14:49 -0400

    Update basic.js

--- basic.js
+++ basic.js
@@ -779,3 +779,27 @@ function checkAge(element){
 function print(element){
    console.log(element)
 }
+
+
+
+// sort an array of numbers
+   
+let grades = [100, 50, 30, 60, 80, 70];
+
+
+grades = grades.sort(descendingSort)
+grades = grades.sort(aescendingSort)
+
+grades.forEach(print);
+
+function descendingSort(x, y){
+   return y - x;
+}
+
+function aescendingSort(x, y){
+   return x - y;
+}
+
+function print(element){
+   console.log(element);
+}
