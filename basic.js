f6c637985b8623d0de14446150f263687cb64047
Pouyan <154380480+Mrengin1@users.noreply.github.com>
2024-07-04 16:23:25 -0400

    Update basic.js

--- basic.js
+++ basic.js
@@ -826,7 +826,22 @@ const percent = (x, y) => x / y * 100;
 console.log(`${percent(2, 10)}%`)
                   */
 
-let grades = [100, 50, 90, 60, 80, 70];
+let cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
-grades.sort((x, y) => x - y);
-grades.forEach((element) => console.log(element))
+shuffle(cards);
+console.log(cards)
+
+function shuffle(array){
+   let currentIndex = array.length;
+
+   while(currentIndex != 0){
+      let randomIndex = Math.floor(Math.random() * array.length);
+      currentIndex-=1;
+
+      let temp = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temp;
+   }
+   return array;
+}
+      
