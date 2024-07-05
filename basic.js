550fddab1600b3fa68d1a37a7b4114d6cf34f0cd
Pouyan <154380480+Mrengin1@users.noreply.github.com>
2024-07-05 19:14:24 -0400

    Update basic.js

--- basic.js
+++ basic.js
@@ -895,3 +895,56 @@ console.log(store.has("underwear"));
 console.log(store.size);
 
 store.forEach((value, key) => console.log(`${key} ${value}`))
+
+
+
+//---------------------------------------
+//OOP
+//object = A group of properties and methods
+//         properties = what an object has 
+//         methods = what an object can do 
+//         use . to access properties/methods
+
+const car1 = {
+   model:"Mustang",
+   color:"red",
+   year:2023,
+
+   drive : function(){
+      console.log("You drive the car");
+   },
+
+   brake : function(){
+      console.log("You step in the brakes");
+   }
+}
+
+const car2 = {
+   model:"Nissan",
+   color:"black",
+   year:2024,
+
+   drive : function(){
+      console.log("You drive the car");
+   },
+
+   brake : function(){
+      console.log("You step in the brakes");
+   }
+}
+
+car1.drive()
+car1.brake()
+
+console.log(car1.model);
+console.log(car1.color);
+console.log(car1.year);
+
+console.log("------------------------------")
+
+car2.drive()
+car2.brake()
+
+console.log(car2.model);
+console.log(car2.color);
+console.log(car2.year);
