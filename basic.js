c51ee6caae192355cf63fa454b62ae47f6ddf1f0
Pouyan <154380480+Mrengin1@users.noreply.github.com>
2024-07-09 21:05:15 -0400

    Update basic.js

--- basic.js
+++ basic.js
@@ -998,3 +998,33 @@ car2.brake()
 console.log(car2.model);
 console.log(car2.color);
 console.log(car2.year);
+
+
+
+//-------------------------------------------------------------------------------------
+//class = a blue print for creating cbjects 
+//        define what properties and methods the have
+//        use a constuctor for unique properties
+
+class Player{
+   score = 0;
+
+   pause(){
+      console.log("you paused the game");
+   }
+   exit(){
+      console.log("you exited the game");
+   }
+
+}
+
+const player1 = new Player();
+const player2 = new Player();
+const player3 = new Player();
+const player4 = new Player();
+
+player1.score += 1;
+
+console.log(player1.score);
+player1.pause();
+player1.exit();
