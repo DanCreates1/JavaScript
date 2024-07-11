6b6fca78d475492a9beabe50ca66017e9029e48d
Pouyan <154380480+Mrengin1@users.noreply.github.com>
2024-07-11 15:11:17 -0400

    Update basic.js

--- basic.js
+++ basic.js
@@ -1159,3 +1159,63 @@ console.log(`The Hawk is alive = ${hawk.alive}`)
 hawk.eat();
 hawk.sleep();
 hawk.fly();
+
+
+
+// super keyWord = Refers to the parent class.
+//                 commonly used to invoke constructor of a parent class
+
+
+class Animal{
+   constructor(name, age){
+      this.name = name;
+      this.age = age;
+   }
+}
+
+class Rabbit extends Animal{
+   
+   constructor(name, age, runSpeed){
+      super(name, age);
+      this.runSpeed = runSpeed;
+   }
+}
+
+
+class Fish extends Animal{
+
+    
+   constructor(name, age, swimSpeed){
+      super(name, age);
+      this.swimSpeed = swimSpeed;
+   }
+}
+
+
+class Hawk extends Animal{
+
+   constructor(name, age, flySpeed){
+      super(name, age);
+      this.flySpeed = flySpeed;
+   }
+}
+
+const rabbit = new Rabbit("rabbit", 1, 40);
+const fish = new Fish("fish", 2, 80);
+const hawk = new Hawk("hawk", 3, 200);
+
+console.log(rabbit.name);
+console.log(rabbit.age);
+console.log(rabbit.runSpeed);
+
+console.log("----------------------------------------");
+
+console.log(fish.name);
+console.log(fish.age);
+console.log(fish.swimSpeed);
+
+console.log("----------------------------------------");
+
+console.log(hawk.name);
+console.log(hawk.age);
+console.log(hawk.flySpeed);
