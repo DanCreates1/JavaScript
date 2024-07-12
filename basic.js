a4dd479c03952e88c414bdab18e62f234c8d1716
Pouyan <154380480+Mrengin1@users.noreply.github.com>
2024-07-12 19:50:36 -0400

    Update basic.js

--- basic.js
+++ basic.js
@@ -1232,50 +1232,44 @@ console.log(hawk.flySpeed);
    console.log(x)
 }
    */
+class Rectangle {
 
-class Rectanlge{
-
-   constructor(widht, height){
-      this.widht = widht;
+   constructor(width, height) {
+      this.width = width;
       this.height = height;
    }
 
-   set widht(newWidth){
-      if(newWidth > 0){
-         this._width = newWidth
-      }
-      else{
-         console.error("Width must be a postive number")
-      }
-   }
-
-   set height(newHeight){
-      if(newHeight > 0){
-         this._height = newHeight
-      }
-      else{
-         console.error("Height must be a postive number")
+   set width(newWidth) {
+      if (newWidth > 0) {
+         this._width = newWidth;
+      } else {
+         console.error("Width must be a positive number");
       }
    }
 
-   get widht(){
+   set height(newHeight) {
+      if (newHeight > 0) {
+         this._height = newHeight;
+      } else {
+         console.error("Height must be a positive number");
+      }
+   }
+
+   get width() {
       return `${this._width.toFixed(1)}cm`;
    }
 
-   get height(){
+   get height() {
       return `${this._height.toFixed(1)}cm`;
    }
 
-   get area(){
-      return `${(this.widht * this._height).toFixed(1)}cm^2`
-   } 
+   get area() {
+      return `${(this._width * this._height).toFixed(1)}cm^2`;
+   }
 }
 
+const rectangle = new Rectangle(5, 6);
 
-const rectanlge = new Rectanlge(5, 6);
-
-//rectanlgeArea(3, 4)
-
-console.log(rectanlge.widht);
-console.log(rectanlge.height);
-console.log(rectanlge.area);
+console.log(rectangle.width); 
+console.log(rectangle.height);
+console.log(rectangle.area); 
