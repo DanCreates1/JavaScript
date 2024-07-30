4ff36f8e71220640a46828754208a91e5eb17c34
Dan <154380480+DanCreates1@users.noreply.github.com>
2024-07-30 15:20:17 -0400

    Update basic.js

--- basic.js
+++ basic.js
@@ -1521,3 +1521,60 @@ console.log(person.fullName);
 console.log(person.age);
 console.log(person.isStudent);
 console.log(person.hobbies[2]);
+
+
+
+//Arrays of objects
+
+const fruits = [{name: "apple", color: "red", calories: 95},
+                {name: "orange", color: "orange", calories: 45},
+                {name: "banana", color: "yellow", calories: 105},
+                {name: "coconut", color: "white", calories: 59},
+                {name: "pineapple", color: "yellow", calories: 37}];
+
+console. log(fruits[2].name)
+
+
+//push() to add something
+//pop() to remove something
+
+
+//------------forEach()----------------
+fruits.forEach(fruits => console.log(fruits.name))
+
+
+//----------------map()---------------
+
+const fruitNames = fruits.map(fruit => fruit.name);
+const fruitColors = fruits.map(fruit => fruit.color);
+const fruitCalories = fruits.map(fruit => fruit.calories);
+
+
+console.log(fruitNames);
+console.log(fruitColors);
+console.log(fruitCalories);
+
+//-----------filter()--------------
+
+const yellowFruits = fruits.filter(fruit => fruit.color === "yellow");
+const lowCalFruits = fruits.filter(fruit => fruit.calories < 100);
+const highCalFruits = fruits.filter(fruit => fruit.calories >= 100);
+
+console.log(yellowFruits);
+console.log(lowCalFruits);
+console.log(highCalFruits);
+
+
+//------------reduce----------------
+const maxFruits = fruits.reduce((max, fruit) =>
+                                 fruit.calories > max.calories ?
+                                 fruit : max);
+
+
+
+const minFruits = fruits.reduce((min, fruit) =>
+                                 fruit.calories < min.calories ?
+                                 fruit : min);
+
+console.log(maxFruits);
+console.log(minFruits);
