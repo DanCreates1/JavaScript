fe533a6c82165a7d1c913958c191c9ea3e8094fa
Dan <154380480+DanCreates1@users.noreply.github.com>
2024-08-01 13:01:08 -0400

    Update basic.js

--- basic.js
+++ basic.js
@@ -1578,3 +1578,30 @@ const minFruits = fruits.reduce((min, fruit) =>
 
 console.log(maxFruits);
 console.log(minFruits);
+
+
+
+// sort() = method used to sort elements of an array in place
+//          sorts elements as strtings in lexicographic prder, not alphabetical
+//          lexicographic = (alphabet + numbers + symbols) as strings
+/*
+let numbers = [1, 4, 2, 3, 6, 5, 10, 9, 7, 8];
+
+numbers = numbers.sort((a, b) => a - b);
+
+console.log(numbers);
+*/
+//---------------------------------------------
+const people = [
+   {name: "Spongebob", age: 30, gpa: 3.0},
+   {name: "Patrick", age: 37, gpa: 1.5},
+   {name: "Squidward", age: 51, gpa: 2.5},
+   {name: "Sandy", age: 27, gpa: 4.0}
+ ];
+ 
+ people.sort((a, b) => b.age - a.age);
+
+ peopleNameSorted = people.sort((a, b) => b.name.localeCompare(a.name))
+ 
+ console.log(people);
+ 
