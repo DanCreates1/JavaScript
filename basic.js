91314ee8a9a279ec2afc00afa052937a145e3450
Dan <154380480+DanCreates1@users.noreply.github.com>
2024-08-09 13:33:07 -0400

    Update basic.js

--- basic.js
+++ basic.js
@@ -2082,3 +2082,65 @@ const children = element.children;
 Array.from(children).forEach(child => {
    child.style.backgroundColor = "yellow"
 })
+
+
+
+
+
+//62.Add % change HTML
+
+//------------- EXAMPLE 1 <h1> -----------
+
+// STEP 1 CREATE THE ELEMENT
+const newH1 = document.createElement("h1");
+
+// STEP 2 ADD ATTRIBUTES/PROPERTIES
+newH1.textContent = "I like pizza!";
+newH1.id = "myH1";
+newH1.style.color = "tomato";
+newH1.style.textAlign = "center";
+
+// STEP 3 APPEND ELEMENT TO DOM
+document.body.append(newH1);
+//document.body.prepend(newH1);
+//document.getElementById("box4").append(newH1);
+//document.getElementById("box4").prepend(newH1);
+
+
+//const box2 = document.getElementsByTagName("box2");
+//document.body.insertBefore(newH1, box1)
+
+
+//const boxes = document.querySelectorAll(".box");
+//document.body.insertBefore(newH1, boxes[1])
+
+// REMOve HTML ELEMENT
+document.getElementById("box1").removeChild(newH1);
+//html👇🏼
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <title>JavaScript</title>
+  </head>
+  <body>
+   <div id="box1" class="box">
+    <p>Box1</p>
+   </div>
+
+   <div id="box2" class="box">
+    <p>Box2</p>
+   </div>
+
+   <div id="box3" class="box">
+    <p>Box3</p>
+   </div>
+
+   <div id="box4" class="box">
+    <p>Box4</p>
+   </div>
+    <script src="script.js"></script>
+  </body>
+</html>
