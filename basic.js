583fc705dc19af75ab24a67e3ac28b6f32884874
Dan <154380480+DanCreates1@users.noreply.github.com>
2024-08-13 09:59:25 -0400

    Update basic.js

--- basic.js
+++ basic.js
@@ -2319,7 +2319,7 @@ myButton.addEventListener("click", event => {
 
 
 
-
+//66
 //Nodelist = Static collection of HTML elements by (id, class, element)
 //           Can be created by using querySelectorAll()
 //           Similar to an array, but no (map, filter, reduce)
