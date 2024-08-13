300f2e5937be96bae0c7f5f9ea37602b5a022af5
Dan <154380480+DanCreates1@users.noreply.github.com>
2024-08-13 09:57:09 -0400

    Update basic.js

--- basic.js
+++ basic.js
@@ -2316,3 +2316,65 @@ myButton.addEventListener("click", event => {
 #myButton{
     font-size: 3rem;
 }
+
+
+
+
+//Nodelist = Static collection of HTML elements by (id, class, element)
+//           Can be created by using querySelectorAll()
+//           Similar to an array, but no (map, filter, reduce)
+//           Nodelist won't update to autoatically reflect changes
+
+let buttons = document.querySelectorAll(".myButtons");
+
+// ADD HTML/CSS PROPERTIES
+
+buttons.forEach(button => {
+   button.style.backgroundColor = "lightgreen";
+   button.textContent += "💀"
+});
+
+// CLICK event listener
+
+buttons.forEach(button => {
+   button.addEventListener("click", event => {
+      event.target.style.backgroundColor = "tomato"
+   });
+});
+
+//mouseover and mouseout
+
+buttons.forEach(button => {
+   button.addEventListener("mouseover", event => {
+      event.target.style.backgroundColor = "black"
+   });
+});
+
+buttons.forEach(button => {
+   button.addEventListener("mouseout", event => {
+      event.target.style.backgroundColor = "lightGreen"
+   });
+});
+
+// ADD AN ELEMENT
+
+const newButton = document.createElement("button"); // STEP 1
+newButton.textContent = "bUTTON 5"; // STEP 2
+newButton.classList = "myButtons"; 
+document.body.appendChild(newButton); // STEP 3
+
+//how to add the new button to the NodeList👇🏼
+buttons = document.querySelectorAll(".myButtons");
+
+console.log(buttons);
+   
+
+// REMOVE AN ELEMENT
+
+buttons.forEach(button => {
+   button.addEventListener("click", event => {
+      event.target.remove();
+      buttons = document.querySelectorAll(".myButtons");
+      console.log(buttons);
+   });
+});
