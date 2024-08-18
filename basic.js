03712d6dcbf7738d92ff076e43cc240f2b47a556
Dan <154380480+DanCreates1@users.noreply.github.com>
2024-08-18 12:27:46 -0400

    Update basic.js

--- basic.js
+++ basic.js
@@ -2568,3 +2568,70 @@ task1(() => {
 })
 
 
+
+
+//Promise = An object that manages asynchronous operations.
+//            Wrap a Promise object around {asynchronous code}
+//            "I promise to return a value"
+//            PENDING -> RESOLVED or REJECTED
+//            new Promise((resolve, reject) => {asynchronous code})
+
+// DO this chores in order
+
+// 1. WALK THE DOG
+// 2. CLEAN THE KITCHEN
+// 3. TAKE OUT THE TRASH 
+
+function walkDog(){
+
+   
+   return new Promise((resolve, reject) => {
+      setTimeout(() => {
+
+         const dogwalked =true;
+         if(dogwalked){
+            resolve("you walk the dog");
+         }
+         else{
+            reject("you didn't walk the dog");
+         }
+         
+      }, 1500);
+   })
+}
+
+function cleanKitchen(){
+   return new Promise((resolve, reject) => {
+      setTimeout(() => {
+
+         const kitchenCleaned = true;
+
+         if(kitchenCleaned){
+         resolve("you clean the kitchen");
+         }
+         else{
+         reject("you didn't clean the kitchen")
+         }
+      }, 2500)
+   })
+}
+
+function takeOutTrash(){
+   return new Promise((resolve, reject) => {
+      setTimeout(() => {
+         const trashTakenOut = false;
+         if(trashTakenOut){
+            resolve("you take outh the trash");
+         }
+         else{
+            reject("you didn't take out the trash")
+         }
+         
+      }, 1500)
+   })
+}
+
+walkDog().then( value => {console.log(value); return cleanKitchen()})
+         .then(value => {console.log(value); return takeOutTrash()})
+         .then(value => {console.log(value); console.log("you finshed all the tasks")})
+         .catch(error => console.error(error));
