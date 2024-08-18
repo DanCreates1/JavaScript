cb013edc349e85f688a387c537833b952c064a68
Dan <154380480+DanCreates1@users.noreply.github.com>
2024-08-18 13:34:52 -0400

    Update basic.js

--- basic.js
+++ basic.js
@@ -2719,3 +2719,38 @@ async function doChores(){
 }
 
 doChores()
+
+
+
+
+
+
+
+
+
+
+
+//74
+//----------------------------------------------------------------------------------------------------
+//JSON = (JavaScript Object Notation) data-interchange format
+//       used for exchanging data between a server and a web application
+//       JSON files {Key;value} OR [value1, value2, calue3]
+
+//       JSON.stringify() = converts a JS object to a JSON string.
+//       JSON.parse() = converts a JSON string to a JS object
+
+const jsonNames = `["Spongebob", "Patrick", "Squidward"]`;
+const jsonPerson = `{"name":"Spongebob","age": 30,"isEmployed": true,"hobies":["jellyFishing", "Karate", "Cooking"]}`
+const jsonPeople = `[{"name": "Spongebob", "age": 30,"isEmployed": true},
+                     {"name": "Squidward", "age": 50,"isEmployed": true},
+                     {"name":"Sandy", "age": 27,"isEmployed": false }]`;
+               
+const parseData = JSON.parse(jsonPeople);
+
+//console.log(parseData)
+
+// fetch👇🏼
+fetch("person.json")
+     .then(response => response.json())
+     .then(value => console.log(value))
+     .catch(error => console.error(error))
