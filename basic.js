70bedb78ae21456ede07a3b8559f491a35bb0708
Dan <154380480+DanCreates1@users.noreply.github.com>
2024-08-20 15:06:09 -0400

    Update basic.js

--- basic.js
+++ basic.js
@@ -2754,3 +2754,81 @@ fetch("person.json")
      .then(response => response.json())
      .then(value => console.log(value))
      .catch(error => console.error(error))
+
+
+
+
+
+
+
+
+
+
+
+
+//-------------------------------------------------------------------------------------------------------
+// fetch = Function used for making HTTP requests to fetch resources.
+//         (JSON style data, images, files)
+//         Simplifies asynchronous data fetching in JavaScript and
+//         used for interacting with APIs to retrieve and send
+//         data asynchronously over the web
+//         fetch(url, {options})
+/*
+fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+      .then(response => {
+
+         if(!response.ok){
+            throw new Error("Could not fetch resource");
+         }
+         return response.json();
+
+      })
+      .then(data => console.log(data))
+      .catch(error => console.error(error));
+      ;                                         */
+
+fetchData()
+async function fetchData() {
+ 
+   try{
+      const pokemonName = document.getElementById("pokemonName").value.toLocaleLowerCase();
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+
+      if(!response.ok){
+         throw new Error("Could not fetch resource")
+      }
+
+      const data = await response.json();
+      const pokemonSprite = data.sprites.front_default;
+      const imgElement = document.getElementById("pokemonSprite");
+
+      imgElement.src = pokemonSprite;
+      imgElement.style.display = "block"
+   }
+   catch(error){
+      console.error(error);
+   }
+}
+
+
+//HTML👇🏼
+/*
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <title>JavaScript</title>
+  </head>
+  <body>
+
+    <input type="text" id="pokemonName" placeholder="Enter pokemon name">
+    <button onclick="fetchData()">Fetch Pokemon</button>
+
+    <img src="" id="pokemonSprite" style="display: none;">
+
+    <script src="script.js"></script>
+  </body>
+</html>
+*/
