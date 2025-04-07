cbff65f530cf350c5571a3af15f1b65b35ccfe60
pouyan nazarzade <poyannazarzade@gmail.com>
2025-04-07 19:03:31 -0400

    imporved plan.md

--- Java script projects/cars/src/CarsData.js
+++ Java script projects/cars/src/CarsData.js
@@ -1,8 +1,18 @@
+
 export default [
     {
-        
-    },
-    {
-
+     Image: "https://images.unsplash.com/photo-1611859266238-4b98091d9d9b?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+     Name: "Gtr R35",
+     Price: 200000,
+     Engine: "3.8L V6 Twin-Turbo",
+     Horsepower: 565,
+     Torque: 467,
+     TopSpeed: "315 km/h",
+     to60: 2.9,
+     Weight: 3820,
+     FuelEconomy: 16,
+     Transmission: "6-Speed Dual-Clutch",
+     Drivetrin: "AWD",
+     Description: "The Nissan GT-R R35 is a high-performance sports car that has become an icon in the automotive world. Known for its powerful twin-turbocharged V6 engine, advanced all-wheel-drive system, and cutting-edge technology, the GT-R offers exhilarating speed and handling. Its aggressive design and aerodynamic features contribute to its striking presence on the road. With a focus on performance and precision engineering, the GT-R R35 has earned a reputation as one of the most capable and thrilling cars in its class."  
     }
 ]
--- Java script projects/cars/src/plan.md
+++ Java script projects/cars/src/plan.md
@@ -4,3 +4,23 @@ TASKS:
     1. create the carFlashCard.jsx
     2. Map over it
     3. Make it reusable 
+    4.create a simple database
+
+
+DB/flashcard contents:
+    Image(API pull)
+    Name
+    0-100km/h
+    price
+    Engine
+    Fuel type
+    Fuel capacity
+    Horsepower
+    Torque
+    TopSpeed
+    0to60
+    Weight
+    FuelEconomy
+    Transmission
+    Drivetrin
+    Description
