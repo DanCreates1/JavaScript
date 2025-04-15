85e4fd2a0b9910cfb6c122d258dba3c8a633d66c
pouyan nazarzade <poyannazarzade@gmail.com>
2025-04-15 19:47:00 -0400

    improved the data set a little bit, try to add css grid

--- Java script projects/cars/src/CarsData.js
+++ Java script projects/cars/src/CarsData.js
@@ -16,7 +16,6 @@
     Lexus
 
 2. Germany
-
     BMW
     Mercedes-Benz
     Audi
@@ -24,33 +23,29 @@
     Porsche
     
 
-
 */
-
-
-
-
-
-
 
 
 
 
 export default [
     {
-     Name: "Gtr R35",
-     Image: "https://images.unsplash.com/photo-1611859266238-4b98091d9d9b?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-     Price: 200000,
-     Engine: "3.8L V6 Twin-Turbo",
-     Horsepower: 565,
-     Torque: 467,
-     TopSpeed: "315 km/h",
-     to60: 2.9,
-     Weight: 3820,
-     FuelEconomy: 16,
-     Transmission: "6-Speed Dual-Clutch",
-     Drivetrin: "AWD",
-     Description: "The Nissan GT-R R35 is a high-performance sports car that has become an icon in the automotive world. Known for its powerful twin-turbocharged V6 engine, advanced all-wheel-drive system, and cutting-edge technology, the GT-R offers exhilarating speed and handling. Its aggressive design and aerodynamic features contribute to its striking presence on the road. With a focus on performance and precision engineering, the GT-R R35 has earned a reputation as one of the most capable and thrilling cars in its class."  
+        Name: "Gtr R35",
+        Image: "https://images.unsplash.com/photo-1611859266238-4b98091d9d9b?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        Price: 200000,
+        Engine: "3.8L V6 Twin-Turbo",
+        Horsepower: 565,
+        Torque: 467,
+        TopSpeed: "315 km/h",
+        to60: 2.9,
+        Weight: 3820,
+        FuelEconomy: 16,
+        Transmission: "6-Speed Dual-Clutch",
+        Drivetrin: "AWD",
+        Description: `The Nissan GT-R R35 is a high-performance sports car that has become an icon in the automotive world. 
+                      Known for its powerful twin-turbocharged V6 engine, advanced all-wheel-drive system, and cutting-edge technology, 
+                      the GT-R offers exhilarating speed and handling. Its aggressive design and aerodynamic features contribute to its striking presence on the road. 
+                      With a focus on performance and precision engineering, the GT-R R35 has earned a reputation as one of the most capable and thrilling cars in its class.`
     },
     {
         Name: "Lamborghini Huracan",
@@ -65,7 +60,10 @@ export default [
         FuelEconomy: 15,
         Transmission: "7-Speed Dual-Clutch",
         Drivetrin: "AWD",
-        Description: "The Lamborghini Huracán is a stunning supercar that embodies the essence of Italian automotive excellence. With its aggressive design, powerful V10 engine, and advanced aerodynamics, the Huracán delivers exhilarating performance and breathtaking speed. Its luxurious interior features cutting-edge technology and premium materials, providing a perfect blend of comfort and sportiness. Whether on the track or the open road, the Huracán offers an unforgettable driving experience that captures the spirit of Lamborghini's legendary heritage."
+        Description: `The Lamborghini Huracán is a stunning supercar that embodies the essence of Italian automotive excellence. 
+                      With its aggressive design, powerful V10 engine, and advanced aerodynamics, the Huracán delivers exhilarating performance and breathtaking speed. 
+                      Its luxurious interior features cutting-edge technology and premium materials, providing a perfect blend of comfort and sportiness. 
+                      Whether on the track or the open road, the Huracán offers an unforgettable driving experience that captures the spirit of Lamborghini's legendary heritage.`
     },
     {
         Name: "BMW X5M",
@@ -80,6 +78,10 @@ export default [
         FuelEconomy: 18,
         Transmission: "8-Speed Automatic",
         Drivetrin: "AWD",
-        Description: "The BMW X5 M is a high-performance luxury SUV that combines the practicality of an SUV with the exhilarating performance of a sports car. Powered by a robust twin-turbocharged V8 engine, the X5 M delivers impressive acceleration and handling capabilities. Its aggressive styling, sporty suspension, and advanced technology create a thrilling driving experience. Inside, the X5 M offers a luxurious and spacious cabin with premium materials and cutting-edge infotainment features, making it a perfect blend of performance and comfort for enthusiasts and families alike."
+        Description: `The BMW X5 M is a high-performance luxury SUV that combines the practicality of an SUV with the exhilarating performance of a sports car. 
+                      Powered by a robust twin-turbocharged V8 engine, the X5 M delivers impressive acceleration and handling capabilities. 
+                      Its aggressive styling, sporty suspension, and advanced technology create a thrilling driving experience. 
+                      Inside, the X5 M offers a luxurious and spacious cabin with premium materials and cutting-edge infotainment features, 
+                      making it a perfect blend of performance and comfort for enthusiasts and families alike.`
     }
-]
+];
