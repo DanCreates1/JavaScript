1e875ceab35d9284ed289bc8cd9042924c4b51a7
pouyan nazarzade <poyannazarzade@gmail.com>
2025-04-20 19:50:31 -0400

    addea video

--- Java script projects/cars/src/CarsData.js
+++ Java script projects/cars/src/CarsData.js
@@ -257,7 +257,7 @@ export default [
                       Known for its powerful twin-turbocharged V6 engine, advanced all-wheel-drive system, and cutting-edge technology, 
                       the GT-R offers exhilarating speed and handling. Its aggressive design and aerodynamic features contribute to its striking presence on the road. 
                       With a focus on performance and precision engineering, the GT-R R35 has earned a reputation as one of the most capable and thrilling cars in its class.`,
-        video: "https://www.youtube.com/embed/y9xBDeK6q7c?si=8b1FqTFuL3AVZjaj"
+        video: "https://www.youtube.com/embed/KY0-a-PUU0Q"
     },
     {
         
